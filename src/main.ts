b55d5198c8fab6abaa89e618d799b97197300ad1
Mathieu <mathieu@degouville.com>
2023-11-09 16:34:32 -0500

    Fix floating text behavior and add infinite loop

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './style.css'
 import throttle from 'lodash.throttle'
 
+// NOTE: Force-bypass the 1st Overlay
+window.scroll({ top: window.innerHeight, behavior: 'instant' })
+
 const runApp = () => {
   const app = document.querySelector('#app') as HTMLElement
   const resetButton = document.querySelector('.reset') as HTMLElement
@@ -13,7 +16,7 @@ const runApp = () => {
 
   const effectDuration = 300
   const slidesAmount = Array.from(slideElements).length
-  let currentSlide: number = 0
+  let currentSlide: number = 1 // TODO: use state for the slides
 
   const triggerEffect = () => {
     slides.classList.add('is-scrolling')
@@ -22,21 +25,34 @@ const runApp = () => {
 
   const updateLinks = () => {
     const isActive = app.querySelector('.is-active') as HTMLElement
-    const currentLink = Array.from(linkElements)[currentSlide]
-
+    // TODO: improve this with status value
+    const currentLink = Array.from(linkElements)[currentSlide - 1] 
+    // TODO: Make those links clickable to their targets
     isActive?.classList.remove('is-active')
     currentLink?.classList.add('is-active')
   }
 
+  const reload = (top: number = 0, behavior: ScrollBehavior = 'instant') => {
+    console.info('%cRun it back Turbooo ∞💫', 'color: yellow')
+    currentSlide = 0
+    window.scroll({ top, behavior })
+  }
+
   const goToNext = (isReverse: boolean = false, behavior: ScrollBehavior = 'smooth') => {
-    const { innerHeight } = window    
+    const { innerHeight } = window
+    const isFirst = currentSlide >= 1
     
-    if (isReverse && currentSlide > 0) currentSlide--
-    if (!isReverse && slidesAmount > currentSlide) currentSlide++
-    if (!!currentSlide && currentSlide !== slidesAmount) triggerEffect()
+    if (isReverse && isFirst) currentSlide--
+    if (!isReverse && slidesAmount - 1 > currentSlide) currentSlide++
+
+    const isLast = currentSlide === slidesAmount - 1
+    const isOverlay = currentSlide === 0 || currentSlide === slidesAmount
+    if (!isOverlay) triggerEffect()
+
     updateLinks()
 
     window.scroll({ top: currentSlide * innerHeight, behavior })
+    if (isLast) setTimeout(reload, effectDuration * 3)
   }
 
   const animate = (e: KeyboardEvent | MouseEvent | WheelEvent) => {
@@ -51,22 +67,22 @@ const runApp = () => {
     if (code === keys[1]) goToNext()
   }
 
-  const moveMouseText = (e: MouseEvent) => {
-    floatingText.style.display = 'block';
-    floatingText.style.left = `${e.screenX + 10}px`;
-    floatingText.style.top = `${e.screenY - 70}px`;
-  }
+  const moveMouseText = throttle((e: MouseEvent) => {
+    const x = e.offsetX + 20
+    const y = e.offsetY - 10
+    floatingText.style.display = 'block'
+    floatingText.style.transform = `translate(${ x }px, ${ y }px)`
+  }, 50)
 
   const reset = () => {
-    currentSlide = 0
+    currentSlide = 2
     goToNext(true)
   }
   const resetFinally = () => setTimeout(reset, 0)
 
-
-  // TODO: Handle animation on scroll
   window.addEventListener('wheel', e => e.preventDefault(), {passive: false})
-  window.addEventListener('wheel', throttle(animate, 1000))
+  // TODO: Find a remove throttle and just block any event while animate() is running
+  window.addEventListener('wheel', throttle(animate, effectDuration * 3))
   window.onmousemove = moveMouseText
   window.onkeydown = animate
   window.onclick = animate
